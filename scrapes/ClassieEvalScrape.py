81818542b228779e88a16445409e9e79252bde6d
James Angeles <JamesAngeles21@gmail.com>
2019-03-04 21:47:52 -0500

    implemented classie login and set up webdriver libraries

---
+++ scrapes/ClassieEvalScrape.py
@@ -0,0 +1,22 @@
+import re
+import credentials
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+def login(site, userElement, passwordElement, submitElement):
+    driver.get(site)
+    username = driver.find_element_by_id(userElement)
+    password = driver.find_element_by_id(passwordElement)
+    username.send_keys(credentials.username)
+    password.send_keys(credentials.password)
+    submitBtn = driver.find_element_by_class_name(submitElement)
+    submitBtn.click()
+
+options = Options()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+login("https://classie-evals.stonybrook.edu", "username", "password", "login-button");
